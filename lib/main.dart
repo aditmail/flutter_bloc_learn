1e86e8137604444c72adee4292e77d7dbde24797
aditya ismail <aditmail321@gmail.com>
2020-05-11 10:51:00 +0700

    Change title of Apps

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'ChuckData Norris',
+      title: 'ChuckData Norris with Bloc Implementation',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
